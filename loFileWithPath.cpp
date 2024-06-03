af2d4754c675ae74b9de08481cf42ea9149935dd
HaniefuL <haniefulgeofarry@gmail.com>
2024-06-03 15:00:53 +0700

    menambahkan if (baris == "q") break; untuk menulis dan memasukan nilai dari 'baris' ke dalam file

--- loFileWithPath.cpp
+++ loFileWithPath.cpp
@@ -6,4 +6,24 @@ using namespace std;
 int main() {
     string baris;
     string NamaFile;
+
+    cout << "Masukan nama file : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "_";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+    }
 }
